43cf141a5932991f3979651d97f34ed9e7df31fb
Justin Lee <ju3tinlee@gmail.com>
2025-11-19 11:42:52 -0800

    Updated formating

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,11 @@
 import { drawEquityMap } from "./chart-equity-map.js";
 import { drawStationMap } from "./chart-station-map.js";
 import { drawHeatmap } from "./heatmap.js";
+import { drawDurationChart } from "./chart-duration.js";
 
 document.addEventListener('DOMContentLoaded', async () => {
   drawStationMap();
   drawHeatmap();
   drawEquityMap();
+  drawDurationChart();
 });
